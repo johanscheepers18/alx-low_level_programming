7563930727c7b4cbd0f61e84ffc9a17ea61bbadc
johanscheepers18 <johanscheepers14@gmail.com>
2023-06-15 14:17:20 -0700

    Added comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-/*This function main is used to prints a string with the value of Programming is like building a multilingual puzzle. 
- *
- * Return: Always 0*/
+/**
+*main - Entry point
+*Description: This program is used to print a certain string
+*Retrun: Always 0 (Success)
+*/
 int main(void)
 {
+	/*Puts: is used to print the string to the console*/
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
