c5b2f1e827fe00f7630b7b9c569b85835cab1d28
johanscheepers18 <johanscheepers14@gmail.com>
2023-06-15 13:25:03 -0700

    Fixed spelling mistake in puts() out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-/*This function main is used to print a certain string. */
+/*This function main is used to prints a string with the value of Programming is like building a multilingual puzzle. 
+ *
+ * Return: Always 0*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingial puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
