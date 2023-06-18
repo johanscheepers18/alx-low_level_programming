659424e554bdb53bdacfc1cdad6be7c3a3be7296
johanscheepers18 <johanscheepers14@gmail.com>
2023-06-18 13:53:38 -0700

    Fixed indenting issues

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-        char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
 
-        int i;
+	int i;
 
-        for (i = 0 ; i <= 25 ; i++)
-        {
+	for (i = 0 ; i <= 25 ; i++)
+	{
 		putchar(alpha[i]);
 
-                if (alpha[i] == 'z')
-                {
-                        putchar('\n');
-                }
-        }
+		if (alpha[i] == 'z')
+		{
+		putchar('\n');
+		}
+	}
 
-        return (0);
+	return (0);
 }
