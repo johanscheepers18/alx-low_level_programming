0d00ded7a32f6d3126e1e40fa8f6c1ea46653b42
johanscheepers18 <johanscheepers14@gmail.com>
2023-06-20 06:31:26 -0700

    Added 2-main.c and 2-print_alphabet_x10. files

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point
+ * Description: Prints the full alphabet ten times
+ */
+void print_alphabet_x10(void)
+{
+	int i, x;
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (x = 'a' ; x <= 'z' ; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
