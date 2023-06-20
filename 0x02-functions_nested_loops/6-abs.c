9cdf97e6363eca5c93d35930501738bcfa8abbf7
johanscheepers18 <johanscheepers14@gmail.com>
2023-06-20 11:37:12 -0700

    Added comments to 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * _abs - Entry point
+ * @x: used as the number to get the absolute value
+ * Return: returns the absolute value of x
+ */
 int _abs(int x)
 {
 	return (abs(x));
