0d33378f2d298eb08d540d9426b69d507f1f1afd
johanscheepers18 <johanscheepers14@gmail.com>
2023-06-20 13:28:10 -0700

    Added 9-times_table.c and 9-main.c files, updated main.h

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * times_table - entry point
+ * Description: This program is used to print the 9 * 9 times table
+ */
+void times_table(void)
+{
+	int x, y, z;
+	
+	x = 0;
+
+	while (x < 10)
+	{
+		y = 0;
+
+		while (y < 10)
+		{
+			z = x * y;
+
+			if (y == 0)
+			{
+				_putchar(z + '0');
+			}
+
+			if (z < 10 && y != 0)
+			{
+				_putchar(',');
+                        	_putchar(' ');
+                        	_putchar(' ');
+				_putchar(z + '0');
+			}
+
+			else if (z >= 10)
+			{
+				_putchar(',');
+                        	_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			y++;
+		}
+
+		_putchar('\n');
+		x++;
+	}
+}
