24dd630615e502f9e7ada8d25012b60a95927107
johanscheepers18 <johanscheepers14@gmail.com>
2023-06-20 12:52:40 -0700

    Added comments to 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,9 @@
 #include "main.h"
 
+/**
+ * jack_bauer - Entry point
+ * Description: This program is used to print time starting from 00:00 to 23:59
+ */
 void jack_bauer(void)
 {
 	int hours, minutes;
