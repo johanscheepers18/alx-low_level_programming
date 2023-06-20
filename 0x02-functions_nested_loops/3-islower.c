5296e81361144cc91e9f2b909a765e8fc4d7f523
johanscheepers18 <johanscheepers14@gmail.com>
2023-06-20 10:43:36 -0700

    Added comments to 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <ctype.h>
 
+/**
+ * _islower - Entry point, and uses variabe c as the input
+ * Description: this program is used to return 1 if the input is lowercase,
+ * or 0 if other.
+ * Return: 1 if lowercase, 0 if other
+ */
 int _islower(int c)
 {
 	if (islower(c))
