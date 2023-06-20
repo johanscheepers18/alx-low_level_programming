77b62b8b67b3af1d72fcc4e5854436fcfee5b99a
johanscheepers18 <johanscheepers14@gmail.com>
2023-06-20 14:16:24 -0700

    Added 10-main.c and 10-add.c, updated main.h file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ * Description: this program is used to add two integers
+ * Return: returns the answer of x + y
+ */
+int add(int x, int y)
+{
+	int answer = x + y;
+
+	return (answer);
+}
