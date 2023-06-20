cac2a8c818e031e5ae9a53d5eee8b4297df4c95e
johanscheepers18 <johanscheepers14@gmail.com>
2023-06-20 04:40:20 -0700

    Updated 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar(i);
+	}
+
+	-putchar('\n');
+}
