4ca2ca33694a5d7edcbde0fbca1bc416523ab78f
johanscheepers18 <johanscheepers14@gmail.com>
2023-06-22 13:16:50 -0700

    Added 7-print_diagonal.c, 7-main.c files, and updated main.h

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line in the terminal
+ * @n : the width of the diagonal line
+ */ 
+void print_diagonal(int n)
+{
+	int x, y;
+	
+	if (n > 0)
+	{
+		for (x = 0 ; x < n ; x++)
+		{
+			for (y = 0 ; y < x ; y++)
+			{
+				_putchar(' ');
+			}
+
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
