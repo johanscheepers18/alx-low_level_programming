d6bc6ef31f38c082b0fc54f574940acf63fa1f64
johanscheepers18 <johanscheepers14@gmail.com>
2023-06-22 11:51:27 -0700

    Added 3-print_numbers.c again

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints all natural numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+		_putchar(x + '0');
+	}
+
+	_putchar('\n');
+}
