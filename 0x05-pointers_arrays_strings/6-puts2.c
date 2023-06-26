ebbfb0436e43fe63e4572a66564c0d70e24f1a8e
johanscheepers18 <johanscheepers14@gmail.com>
2023-06-26 09:14:44 -0700

    Fixed bugs

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,18 +6,32 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i, x;
+	
+	x = 0;
 
 	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
+		x++;
+	}
 
-		if (str[i + 1] == '\0')
+	if (x > 0)
+	{
+		for (i = 0 ; str[i] != '\0' ; i++)
 		{
-			_putchar('\n');
+			if (i % 2 == 0)
+			{
+				_putchar(str[i]);
+			}
+
+			if (str[i + 1] == '\0')
+			{
+				_putchar('\n');
+			}
 		}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
