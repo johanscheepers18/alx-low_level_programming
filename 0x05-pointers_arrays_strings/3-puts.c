0f7dc1747f6946c9d7796917e16ddeda8e04f17e
johanscheepers18 <johanscheepers14@gmail.com>
2023-06-26 05:49:09 -0700

    Updated 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,14 +6,28 @@
  */
 void _puts(char *str)
 {
-	int i;
+	int i, x;
+
+	x = 0;
 
 	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		_putchar(str[i]);
-		if (str[i + 1] == '\0')
+		x++;
+	}
+
+	if (x > 0)
+	{
+		for (i = 0 ; str[i] != '\0' ; i++)
 		{
-			_putchar('\n');
+			_putchar(str[i]);
+			if (str[i + 1] == '\0')
+			{
+				_putchar('\n');
+			}
 		}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
