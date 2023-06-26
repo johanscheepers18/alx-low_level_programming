e5deb6aa3adbf057611198e59137674e5c2bfbbe
johanscheepers18 <johanscheepers14@gmail.com>
2023-06-26 10:33:17 -0700

    Added comments

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_array - prints n amount of values from an array
+ * @a: the array that is being used
+ * @n: the amount of values to print from the array
+ */
 void print_array(int *a, int n)
 {
 	int i;
