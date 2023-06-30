12e2a014f6a57df1fa60d08417e1853951075502
johanscheepers18 <johanscheepers14@gmail.com>
2023-06-29 17:43:21 -0700

    Fixed betty styling issues

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,17 @@
 #include "main.h"
 
+/**
+ * _strncat - used to add two strings together,
+ * string two is limited by a certain amount of bytes
+ * @dest: destination string
+ * @src: string that should be added to dest
+ * @n: the amount of bytes allowed from src
+ * Return: return the combined string
+ */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, len1;
-	
+
 	len1 = 0;
 
 	while (dest[len1] != '\0')
@@ -16,7 +24,7 @@ char *_strncat(char *dest, char *src, int n)
 		dest[len1] = src[i];
 		len1++;
 	}
-	
+
 	if (len1 < n)
 	{
 		dest[len1] = '\0';
