9e84fe00ace9d2089b762cde536a119b5f5ebd1e
johanscheepers18 <johanscheepers14@gmail.com>
2023-06-29 18:46:48 -0700

    Added 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings together
+ * @s1: first string
+ * @s2: second string
+ * Return: 0 means the strings are the same,
+ * anything else means they are not the same
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0' && *s2 != '\0') && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
