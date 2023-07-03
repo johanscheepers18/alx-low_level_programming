9e953082e10d1804764f09ad955ba724556bc05d
johanscheepers18 <johanscheepers14@gmail.com>
2023-07-03 15:57:05 -0700

    Fixed bugs

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,16 +8,42 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack != '\0')
-	{
-		if (*haystack == *needle)
-		{
-			return (haystack);
-			needle++;
-		}
+	int i, x, y, z, comp;
+	char *start;
 
-		haystack++;
+	x = 0;
+	y = 0;
+
+	while (needle[x] != '\0')
+	{
+		x++;
+	}
+	if (x == 0)
+	{
+		return (haystack);
 	}
 
+	while (haystack[y] != '\0')
+	{
+		if (haystack[y] == needle[0])
+		{
+			start = &haystack[y];
+			z = y;
+			comp = 0;
+			for (i = 0; i < x; i++)
+			{
+				if (haystack[z] == needle[i])
+				{
+					comp++;
+				}
+				z++;
+			}
+		}
+		if (comp == x)
+		{
+			return (start);
+		}
+		y++;
+	}
 	return ('\0');
 }
