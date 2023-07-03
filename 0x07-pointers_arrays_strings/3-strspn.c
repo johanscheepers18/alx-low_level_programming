1ee2e961ef69d7662204122207523fdb76f1cf46
johanscheepers18 <johanscheepers14@gmail.com>
2023-07-03 14:46:28 -0700

    Added 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - is used to see how many bytes in the first segment of s,
+ * contains the same bytes that appears in accept
+ * @s: the string used
+ * @accept: the bytes that will be used to look for in s
+ * Return: returns the amount of times bytes from accept,
+ * appears in the first segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int x, y, z;
+
+	z = 0;
+
+	for (x = 0; s[x] >= 'a' && s[x] <= 'z'; x++)
+	{
+		for (y = 0; accept[y] != '\0'; y++)
+		{
+			if (s[x] == accept[y])
+			{
+				z++;
+			}
+		}
+	}
+
+	return (z);
+}
