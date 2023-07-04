dff47c3f01c97d04a5f6e154194c8d7c134826ea
johanscheepers18 <johanscheepers14@gmail.com>
2023-07-03 17:22:12 -0700

    Added 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * set_string - swaps one string with another
+ * @s: string to be changed
+ * @to: string used to update s
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
