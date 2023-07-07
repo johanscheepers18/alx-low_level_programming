f2889f1c14e257d54204ebce876aaf318f32b853
johanscheepers18 <johanscheepers14@gmail.com>
2023-07-07 07:35:20 -0700

    Added main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,9 @@
 #include "main.h"
 
+/**
+ * main - entry point
+ * Return: returns 0 on success
+ */
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
