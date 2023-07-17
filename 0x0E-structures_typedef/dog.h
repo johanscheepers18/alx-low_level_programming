65c76e3a52c17ab733f19407696e9fc22ff8b05f
johanscheepers18 <johanscheepers14@gmail.com>
2023-07-17 14:25:28 -0700

    Updated dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - entry point into the structre
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
