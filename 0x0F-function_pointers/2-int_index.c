0236a9f8e2451dce7d3637cd808f3f3102238ce0
johanscheepers18 <johanscheepers14@gmail.com>
2023-07-19 14:01:57 -0700

    Added 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * int_index - prints the index of a value,
+ * that matches requirement of a function
+ * @array: array to used
+ * @size: size of the array
+ * @cmp: function to be used
+ * Return: returns -1 if size is less than zero or no matches
+ *		returns the array index if match was found
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, x;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0 ; i < size ; i++)
+	{
+		x = cmp(array[i]);
+
+		if (x != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
