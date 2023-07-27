b095ba6bb25697d2957567f54fb41479977d4363
johanscheepers18 <johanscheepers14@gmail.com>
2023-07-27 10:29:35 -0700

    Added 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a list
+ * @h: list to be used
+ * Return: returns the amount of elemets
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *n = h;
+	size_t count = 0;
+
+	while (n != NULL)
+	{
+		count++;
+		n = n->next;
+	}
+
+	return(count);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -18,5 +18,6 @@ typedef struct lists
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif
