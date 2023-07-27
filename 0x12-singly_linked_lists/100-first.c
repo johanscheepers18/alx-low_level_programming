38b92ab2535887a023085aa3cc29ec254538bdb7
johanscheepers18 <johanscheepers14@gmail.com>
2023-07-27 12:33:26 -0700

    Fixed betty issues

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-void launch_mess() __attribute__((constructor));
+void launch_mess(void) __attribute__((constructor));
 
 /**
  * launch_mess - message to be printed before main func
  */
-void launch_mess()
+void launch_mess(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 	"I bore my house upon my back!\n");
