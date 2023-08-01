3f0bdb2f62b6db1a51ad1eb00964d85ebd9fbeb4
johanscheepers18 <johanscheepers14@gmail.com>
2023-08-01 16:00:53 -0700

    Fixed the bugs

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,20 +1,24 @@
 #include "lists.h"
 
+/**
+ * pop_listint - removes a node from the list
+ * @head: pointer to the head of the list
+ * Returns: the head node value
+ */
 int pop_listint(listint_t **head)
 {
 	int i;
 	listint_t *x;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 	{
 		return (0);
 	}
 
 	x = *head;
 	i = x->n;
-	free(x);
-
 	*head = (*head)->next;
+	free(x);
 
 	return (i);
 }
