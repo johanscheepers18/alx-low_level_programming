584125c1f8462f561eddcbde8385a0f11bcdda26
johanscheepers18 <johanscheepers14@gmail.com>
2023-08-03 15:10:47 -0700

    Added 1-print_binary.c, updated main.h

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to be used
+ * Return; returns on completion
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int x = 0;
+	int y;
+
+	if (n == 0)
+	{
+		_putchar('0' + 0);
+		return;
+	}
+
+	for (x = n, y = 0 ;  (x >>= 1) > 0 ; y++)
+	{
+		;
+	}
+
+	for (;y >= 0 ; y--)
+	{
+		if ((n >> y) & 1)
+		{
+			_putchar('0' + 1);
+		}
+		else
+		{
+			_putchar('0' + 0);
+		}
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdlib.h>
+#include <stdio.h>
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char x);
 
 #endif
